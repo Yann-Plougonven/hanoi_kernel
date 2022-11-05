08ea1515c5f1ce898acdc9a05c75002a07b84aa3
Yann Plougonven--Lastennet <77398553+Yann-Plougonven@users.noreply.github.com>
2022-11-05 22:42:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/Yann-Plougonven/hanoi_kernel',
     author='Yann Plougonven--Lastennet',
     author_email='plougonven-y@saint-louis29.net',
-    description='The kernel of the hanoi tower game project, made during NSI class',
+    description="Hanoi tower game's kernel",
     packages=find_packages(),    
     install_requires=['Stack'],
 )
